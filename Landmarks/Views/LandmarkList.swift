a6cc91326f8a9e1caa72b98a5b37fbaa3ad41dad
Sanghun <po4865@gmail.com>
2022-09-12 19:45:00 +0200

    feat: Generate previews dynamically
    - Add code to the preview provider to render previews of the list view at different device size.
    - Change the preview device by calling the `previewDevice()` modifier method.

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,15 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(
+            [
+                "iPhone SE (2nd generation)",
+//                "iPhone 13",
+//                "iPad Air (5th generation)"
+            ], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
