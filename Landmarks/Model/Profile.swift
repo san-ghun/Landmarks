f77712d23102d87d35fe66f6be0af2821d8fb78a
Sanghun <po4865@gmail.com>
2022-09-14 14:49:08 +0200

    feat: Display a user profile
    HikeBadge.swift
    - Compose the Badge with some descriptive text about the hike.
    	- The badge is just a graphic, so the text in `HikeBadge` along with `accessibilityLabel()` modifier make the meaning of the badge clearer to other users.
    	- The badge's drawing logic produces a result that depends on the size of the frame in which it renders. To ensure the desired appearance, render in a frame of 300 x 300 points. To get the desired size for the final graphic, then scale the rendered result and place it in a comparably small frame.
    
    Profile.swift
    - Define a user profile with properties.
    
    ProfileHost.swift
    - The view host both a static, summary view of profile information and an edit mode.
    - Display the profile summary view.
    
    ProfileSummary.swift
    - Add a model data environment object, to use the hike data.
    - Take a `Profile` instance, rather than a binding to the profile.
    	- Because, the parent view, `ProfileHost`, manages the state for the view.
    - Display basic user information using VStack in ScrollView.
    - Add several badges with varying hues and resons for earning the badge.
    - Include a `HikeView`.
    
    CategoryHome.swift
    - Add a user profile button to the navigation bar using the `toolbar` modifier, and present the `ProfileHost` view when the user taps it.
    - Add the `listStyle` modifer to pick a list style that better suits the content.

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Sanghun Park on 14.09.22.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case sprint = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
