b2649bd384e160c490056f9b5031e5506463fad8
Sanghun <po4865@gmail.com>
2022-09-16 00:40:58 +0200

    feat: Add a watchOS target
    - Add a watchOS target to the project, and set the watchOS target.
    - Share some resources from the iOS target.
    	- Reuse the app's data model, resource files, and views that both platforms can display without modification.
    	- Disable the entry point for watchOS app, by commenting @main wrapper in `WatchLandmarksApp.swift`
    	- Update the target membership of following files to share the resource, by selecting the checkbox for the Watch App.
    		- LandmarksApp.swift for the shared app definition.
    		- LandmarkList, LandmarkRow, CircleImage, MapView.swift are views that the app can display on watchOS with no changes.
    		- ModelData, Landmark, Hike, Profile.swift that define the app's data model.
    		- landmarkData, hikeData.json, and Assets.xcassets to add resource files loaded by the model.
    - Add a watchOS app icon
    
    LandmarkDetail.swift (for watch app)
    - Add a new custom view to the Watch App.
    - Add the `modelData`, `landmark`, and `landmarkIndex` properties to the new structure.
    - Return a CircleImage view from the body() method.
    - In the preview, create an instance of the model data, and use it to pass a landmark object to the LandmarkDetail structure's initializer.
    	- Set the view's environment object.

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Sanghun Park on 16.09.22.
+//
+
+import SwiftUI
+
+//@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
