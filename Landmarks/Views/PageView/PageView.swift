4f0447c32f3091d6f22b69976663c8a04b948387
Sanghun <po4865@gmail.com>
2022-09-15 22:24:23 +0200

    feat: Add a cutom page control
    PageControl.swift
    - Create and update the type to conform to the UIViewRepresentable protocol.
    	- Add a property to store the number of pages.
    	- Add a binding property to have current page.
    	- Add methods to make and update the PageControl.
    - Create a nested `Coordinator` type, and add a `makeCoordinator()` method to create and return a new coordinator.
    	- Because UIControl subclasses like UIPageControl use the target-action pattern instead of delegation, this `Coordinator` implements an @objc method to update the current page binding.
    - In `makeUIView()`, add the coordinator as the target for the `valueChanged` event, specifying the `updateCurrentPage(sender:)` method as the action to perform.
    
    PageView.swift
    - Replace the text box and button with the page control, switching from a VStack to a ZStack for layout.
    
    CategoryHome.swift
    - Replace the placeholder feature image with the new page view.

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -12,23 +12,11 @@ struct PageView<Page: View>: View {
     @State private var currentPage = 0
     
     var body: some View {
-        VStack {
+        ZStack(alignment: .bottomTrailing) {
             PageViewController(pages: pages, currentPage: $currentPage)
-            Text("Current Page: \(currentPage)")
-            Button {
-                if currentPage == 2 {
-                    currentPage = 0
-                }
-                else {
-                    currentPage += 1
-                }
-            } label: {
-                Label("Next", systemImage: "chevron.right.circle")
-                    .labelStyle(.iconOnly)
-                    .imageScale(.large)
-                    .padding()
-            }
-
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
         }
     }
 }
