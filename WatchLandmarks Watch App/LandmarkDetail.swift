b80e9b28fbaa7c36e40f6ee3973af6970b8b0171
Sanghun <po4865@gmail.com>
2022-09-16 15:11:43 +0200

    feat: Create the detail view
    - Embed the circle image in a VStack, and wrap the vertical stack in a scroll view.
    	- Change the circle image to `scaleToFit()` and add padding.
    	- Display the landmark name and its information below the image.
    	- Add the MapView after a divider.
    - Add a title to the back button.
    
    - Create previews for the large and small watch faces.

--- WatchLandmarks Watch App/LandmarkDetail.swift
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -16,15 +16,52 @@ struct LandmarkDetail: View {
     }
     
     var body: some View {
-        CircleImage(image: landmark.image.resizable())
-            .scaledToFill()
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         let modelData = ModelData()
-        LandmarkDetail(landmark: modelData.landmarks[0])
-            .environmentObject(modelData)
+        
+        Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 7 - 45mm")
+            
+            LandmarkDetail(landmark: modelData.landmarks[1])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+        }
     }
 }
