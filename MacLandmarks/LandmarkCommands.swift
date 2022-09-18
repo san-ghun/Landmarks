4220abe0bde956a0d8ea5946feb539fcb3eb3d8b
Sanghun <po4865@gmail.com>
2022-09-18 20:49:34 +0200

    feat: Add a built-in menu command
    - Add a system-provided command that lets the user toggle the sidebar, to be able to get it back after dragging it closed.
    
    LandmarkCommands.swift
    - Set the file targets to include both macOS and iOS.
    	- Target iOS because the shared LandmarkList will eventually depend on some of the types that defined in the file.
    - Import SwiftUI and add a LandmarkCommands structure that conforms to the Command protocol, with a computed body property.
    - Add a SlidebarCommands command to the body, which is a built-in command set including the command for toggling the sidebar.
    
    LandmarkApp.swift
    - Apply LandmarkCommands using the `command()` scene modifier.
    	- Add a condition around the commands modifier to omit it for the watchOS app.

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Sanghun Park on 18.09.22.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
